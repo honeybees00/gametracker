7e33fd0d46d3915c1f3f10816719596b41425ec0
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-21 08:29:28 -0400

    supabase

--- src/services/GameServices.js
+++ src/services/GameServices.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { createClient } from "@supabase/supabase-js";
+
+const supabase_url = import.meta.env.VITE_SUPABASE_URL;
+
+const supabase_anon_key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabase_url, supabase_anon_key);
+export default supabase;
 
 export const GameServices = () => {
   return (
