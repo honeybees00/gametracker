5e9bb48f293369e0266ff218c5fccadcb114e13e
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-26 01:33:36 -0400

    gameCard

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -7,12 +7,12 @@ export const GameCard = () => {
   
     const GameCard=({game})=>
 
-    <div className='border p-4 rounded shadow flex justify-betwen'>
-      <div className='co'
+    
+      <div className='border p-4 rounded shadow flex justify-betwen'>
         <p>{game.title}</p>
         <p>{game.genre}</p>
       </div>
-    
+
   
 };
 export default GameCard;
