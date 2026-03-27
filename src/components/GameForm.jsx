21086bf54f3b1f8e23e2f64a45ba881c40e675b4
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-27 00:41:48 -0400

    updated gameForm form

--- src/components/GameForm.jsx
+++ src/components/GameForm.jsx
@@ -4,18 +4,28 @@ import React from 'react'
 //anytime onChange is needed import useState it updates user input//
 export const GameForm = (game) => {
   return (
-    //add onchange//
+		//add onchange//
 
-    <div>
-      <div className='container border rounded-xl p-2 bg-gray-700-gray-m-3'>
-      <input type="text"placeholder='Game Title'/>
-      <input type="text"placeholder='Genre'/>
-
-       <input value={(e)=>setForm({GameForm,title:e.target.Value})}/>
-      <button className='success'>Submit</button>
-    </div>
-    </div>
-  )};
+		<div>
+      <form>
+			<div className="container border rounded-xl p-2 bg-gray-700-gray-m-3">
+				<label>title</label>
+				<input type="text" placeholder="Game Title" />
+				<input type="text" placeholder="Genre" />
+				<input
+					value={form.Title}
+					onChange={(e) => setForm({ GameForm, title: e.target.Value })}
+				/>
+				<label>Genre</label>
+				<select
+					onChange={(e) => setform({ GameForm, genre: e.target.Value })}/>
+          <option>Genre</option>
+    <button className="submit">Submit</button>
+			</div>
+      </form>
+		</div>
+    
+	);};
 e.preventDefault;
 
   
