52c7622037b69fba9429c4093c71168eed3cd36a
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-27 01:20:29 -0400

    update gameForm onchange/form

--- src/components/GameForm.jsx
+++ src/components/GameForm.jsx
@@ -1,31 +1,36 @@
-import { useState } from 'react';
-import { title } from 'node:process';
-import React from 'react'
+import { useState } from "react";
+import { title } from "node:process";
+import React from "react";
+import { error } from "node:console";
+const [form, setForm] = useState({
+	title: "",
+	genre: "",
+});
+
 //anytime onChange is needed import useState it updates user input//
 export const GameForm = (game) => {
-  return (
+	return (
 		//add onchange//
 
 		<div>
-      <form>
-			<div className="container border rounded-xl p-2 bg-gray-700-gray-m-3">
-				<label>title</label>
-				<input type="text" placeholder="Game Title" />
-				<input type="text" placeholder="Genre" />
-				<input
-					value={form.Title}
-					onChange={(e) => setForm({ GameForm, title: e.target.Value })}
-				/>
-				<label>Genre</label>
-				<select
-					onChange={(e) => setform({ GameForm, genre: e.target.Value })}/>
-          <option>Genre</option>
-    <button className="submit">Submit</button>
-			</div>
-      </form>
+			<form>
+				<div className="container border rounded-xl p-2 bg-gray-700-gray-m-3">
+					<label>title</label>
+					<input type="text" placeholder="Game Title" />
+					<input
+						value={form.title}
+						onChange={(e) => setForm({ GameForm, title: e.target.value })}/>
+					<option>Game Title</option>
+					<label>Genre</label>
+					<input type="text" placeholder="Genre" />
+					<input
+						value={form.genre}
+						onChange={(e) => setForm({ GameForm, genre: e.target.value })}/>
+					<option>Genre</option>
+					<button className="submit">Submit</button>
+				</div>
+			</form>
 		</div>
-    
-	);};
+	);
+};
 e.preventDefault;
-
-  
