381f3a9e56103da861ee88b9fd1a4f11a5fcd327
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-26 21:47:53 -0400

    updated AddGame imports

--- src/pages/AddGamePage.jsx
+++ src/pages/AddGamePage.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
-import {addgame} from '/services/api'
+import {addgame} from '/services /pages'
 import { title } from 'node:process'
-let 'Addgamepage'game;
-//useState lifecycle methods hook into react//
+import {GameServices}from '/servives'
 
 
 
 export const Addgamepage = async(game) => {
 const [form,setForm]=useState(
-  title:"",
-  genre:"",
+  title,"",
+  genre,"",
 )
   return (
     <div>Addgamepage</div>
