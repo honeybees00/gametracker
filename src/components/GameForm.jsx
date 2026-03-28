7f8b6de2d33da68e051b6b5730c9ba5ba821d20e
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-28 16:52:53 -0400

    adjusted

--- src/components/GameForm.jsx
+++ src/components/GameForm.jsx
@@ -13,31 +13,40 @@ export const GameForm = ({onSubmit}) => {
     onSubmit({title,genre,rating:parseInt(rating)})
   }
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<div className="container border rounded-xl p-2 bg-gray-700-gray-m-3">
+		<div className="container border rounded-xl p-2 bg-gray-700-gray-m-3">
+			<form className=" flex flex-col gap-4" onSubmit={handleSubmit}>
+				
 					<label>title</label>
 					<input
+						className="border p-2 shadow-md mb-3"
 						value={title}
 						onChange={(e) => setTitle(e.target.value)}
 						type="text"
 					/>
 					<label>genre</label>
 					<input
+						className="border p-1 shadow-md mb-3"
 						value={genre}
 						onChange={(e) => setGenre(e.target.value)}
 						type="text"
 					/>
 					<label>rating</label>
 					<input
+						className="border p-1 shadow-md mb-3"
 						value={rating}
 						onChange={(e) => setRating(e.target.value)}
 						type="text"
 					/>
-					<button type="submit">Submit</button>
-				</div>
+					<button
+						className="bg-gray-800 border p-1 shadow-lg hover:bg-gray-500 text-white"
+						type="submit"
+					>
+						Submit
+					</button>
+				
 			</form>
 		</div>
+    
 	);
 };
 
